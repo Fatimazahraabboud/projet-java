55f54161d21ddf504be45dc78c6085c88cefc268
Fatimazahraabboud <58141716+Fatimazahraabboud@users.noreply.github.com>
2020-12-14 17:25:36 +0100

    Update AlertesMeteo.java

--- AlertesMeteo/src/alertesmeteo/AlertesMeteo.java
+++ AlertesMeteo/src/alertesmeteo/AlertesMeteo.java
@@ -11,6 +11,9 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import  org.eclipse.paho.client.mqttv3.MqttException ;
+import  org.eclipse.paho.client.mqttv3.MqttMessage ;
+
 /**
  *
  * @author Fatima zahra
